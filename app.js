e06df0c6025d52ba689023bda5abb336962f7e1a
Steven Graboski <sgrabosk@gmail.com>
2016-04-09 10:56:41 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+/* 0: Foundation */
+
+// requires
+var fs = require('fs');
+var prompt = require('prompt');
+var request = require('request');
+
+// write file
+var w_file = "nyt.json";
+
+/* 1: Prompt */
+
+/* 2: Request */
+
+/* 3: Write File */
