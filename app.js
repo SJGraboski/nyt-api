e2bf17339d9ea741462b78c756c8e87a646a5c9c
Wojciech Pierscionek <PierscionekW@gmail.com>
2016-04-09 11:30:52 -0400

    not fully functional

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* 0: Foundation */
-
+// NYTURL
 // requires
 var fs = require('fs');
 var prompt = require('prompt');
@@ -7,8 +7,23 @@ var request = require('request');
 
 // write file
 var w_file = "nyt.json";
+var userInput = process.argv[2];
 
 /* 1: Prompt */
+prompt.start();
+prompt.get(["Search"], function(err, result){
+	console.log(" welcome team");
+	console.log("something");
+});
+
+function output(article){
+	request("http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + article + "&api-key=51d2ee7ce05435e31212f1d25d0cac79:3:74629311", function(error, response, body){
+		if (!error && response.statusCode == 200){
+			var json = JSON.parse(body);
+			console.log()
+		}
+	})
+}
 
 /* 2: Request */
 
