9f56e850592626a334df61e5a5bf7b92cca846b2
Steven Graboski <sgrabosk@gmail.com>
2016-04-09 11:09:51 -0400

    Piped placeholder API info to file, need to figure out formatting

--- app.js
+++ app.js
@@ -12,4 +12,11 @@ var w_file = "nyt.json";
 
 /* 2: Request */
 
+request
+  .get('http://www.omdbapi.com/?t=goodfellas&tomatoes=true')
+  .on('error', function(err) {
+    console.log(err)
+  })
+  .pipe(fs.createWriteStream(w_file));
+
 /* 3: Write File */
