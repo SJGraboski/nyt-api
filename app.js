81ce1236d222113d713287a6105d138b49c2bcf2
Steven Graboski <sgrabosk@gmail.com>
2016-04-09 11:33:08 -0400

    Ok

--- app.js
+++ app.js
@@ -12,11 +12,20 @@ var w_file = "nyt.json";
 
 /* 2: Request */
 
-request
-  .get('http://www.omdbapi.com/?t=goodfellas&tomatoes=true')
+var nytRequest = request
+  .get("http://api.nytimes.com/svc/search/v2/articlesearch.json?q=trump&api-key=51d2ee7ce05435e31212f1d25d0cac79:3:74629311")
   .on('error', function(err) {
     console.log(err)
   })
   .pipe(fs.createWriteStream(w_file));
+
+nytRequest.on('finish', function() {
+	fs.readFile('nyt.json','utf-8', function(error, data){
+    if (error) throw error;
+    console.log('callback ok');
+    data = JSON.parse(data);
+    console.log(data.response.docs);
+	});
+})
 
 /* 3: Write File */
